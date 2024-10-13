04fbfca7b3a75a7a69efcab9b5e4814423852278
mikukko <im.ifuo@outlook.com>
2024-10-13 12:02:15 +0800

    modify localized sh

--- remove_localized.sh
+++ remove_localized.sh
@@ -1,11 +1,12 @@
 directories=(
-  "/Users/miku/Desktop"
-  "/Users/miku/Documents"
-  "/Users/miku/Downloads"
-  "/Users/miku/Movies"
-  "/Users/miku/Music"
-  "/Users/miku/Pictures"
-  "/Users/miku/Public"
+  "~/Desktop"
+  "~/Documents"
+  "~/Downloads"
+  "~/Movies"
+  "~/Music"
+  "~/Pictures"
+  "~/Public"
+  "~/Applications"
   "/Applications"
 )
 
@@ -13,4 +14,6 @@ for dir in "${directories[@]}"; do
   [[ -f "$dir/.localized" ]] && rm -f "$dir/.localized"
   echo "removed $dir localized"
 done
+
+killall Finder
 

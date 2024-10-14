5db843a4a1c968c266f504a60a1ef58e2e65d0e0
mikukko <im.ifuo@outlook.com>
2024-10-15 01:34:36 +0800

    add icon clean sh

---
+++ icon_cache_clean.sh
@@ -0,0 +1,8 @@
+# 清理图标缓存
+sudo find /private/var/folders/ \( -name com.apple.dock.iconcache -or -name com.apple.iconservices \) -exec rm -rfv {} \;
+
+sudo rm -rf /Library/Caches/com.apple.iconservices.store;
+
+killall Dock
+
+killall Finder
